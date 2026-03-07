10d3f5680174bb5785537d2450c2038d55146d89
Laxman Rayala <laxmanrayala@Laxmans-MacBook-Air.local>
2026-03-07 18:47:36 +0530

    updated the project

--- api/productApi.jsx
+++
@@ -1,8 +0,0 @@
-import api from "./axiosConfig"
-
-const STORE_ID = 1
-
-export const getProducts = async () => {
-  const response = await api.get(`/stores/${STORE_ID}/products`)
-  return response.data
-}
